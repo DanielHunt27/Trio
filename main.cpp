e83fd216d360a37bb6ba5ff05d8a3070cc9c123f
Daniel Hunt <dhunt010@student.butte.edu>
2019-02-20 19:38:34 +0000

    Fixed numbers not sorting in descending order bug and added documentation to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void sortDescending(int,int,int);
 void swap(int&,int&);
 
 int main()
@@ -11,30 +10,30 @@ int main()
   int numA, numB, numC;
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
-
-  sortDescending(numA, numB, numC);
+  
+  // compares all three numbers and sorts them
+  if( numA < numC )
+  {
+    swap(numA,numC);
+  }
+  if( numA < numB )
+  {
+    swap(numA,numB);
+  }
+  if( numB < numC )
+  {
+    swap(numB,numC);
+  }
   
   cout<<"From greatest to least, they are: ";
   cout<<numA<<","<<numB<<","<<numC<<endl;
   return 0;
 }
 
-void sortDescending(int first, int second, int third)
-{
-  if( first < third )
-  {
-    swap(first,third);
-  }
-  if( first < second )
-  {
-    swap(first,second);
-  }
-  if( second < third )
-  {
-    swap(second,third);
-  }
-}
-
+/*
+ * Takes in two ints and swaps them. It uses passby reference, so it doesn't
+ * have a return value.
+ */
 void swap(int &first, int &second)
 {
   int temp = first;
